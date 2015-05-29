d6291625f3ff95103f7695d083a1a93a41ee3ce4
Vlad-true-proger <vladeus007@gmail.com>
2015-05-29 19:17:25 +0300

    ver 1

---
+++ Data.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace TimeManWP
+{
+   public static class Data
+    {
+       public static int Count;
+       public static Repository rep;
+       public static List<Color> colors = new List<Color>()
+       { 
+           
+           Color.FromArgb(250, 230, 63, 70),
+           Color.FromArgb(250, 251, 191, 58),
+           Color.FromArgb(250, 255, 242, 0),
+           Color.FromArgb(250, 163, 246, 81),
+           Color.FromArgb(250, 66,199,20),
+           Color.FromArgb(250, 2, 228, 255),
+           Color.FromArgb(250, 71, 133, 235),
+           Color.FromArgb(250,113, 206, 232 ),
+           Color.FromArgb(250, 146, 72, 255),
+       };
+       
+    }
+}
---
+++ TasksToDate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeManWP
+{
+   public class TasksToDate
+    {
+      public  bool IsDone  { get; set; }
+      public  DateTime Date  { get; set; }
+      List<SimpleTask> tasks = new List<SimpleTask>();     public List<SimpleTask> Tasks
+      {
+          get { return tasks; }
+          set { tasks = value; }
+      }
+    }
+}
